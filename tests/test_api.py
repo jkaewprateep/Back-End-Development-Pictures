ac180b18195808d6dee05fd93d5b8aef6cae5fbf
Jirayu Kaewrpateep <jkaewprateep@gmail.com>
2024-05-24 17:47:41 -0400

    update

--- tests/test_api.py
+++ tests/test_api.py
@@ -90,4 +90,104 @@ def test_delete_picture_by_id(client):
     assert res.status_code == 404
 
 
+###
+# @app.route("{insert URL here}", methods="{insert HTTP method name here}")
+# def {insert method name here}():
+#     return jsonify({insert data list here})
 
+# @app.route("{insert URL here}", methods=["GET"])
+# def {insert method name here}(id):
+#     {enumerate the data list}:
+#         if picture["id"] == id:
+#             return picture
+#     return {"message": "{insert error message here}"}, {insert HTTP_NOT_FOUND_STATUS}
+
+# @app.route("{insert URL here}", methods="insert list of correct method here")
+# def {insert method name here}():
+
+#     # get data from the json body
+#     picture_in = {insert code to get json from the request here}
+
+#     # if the id is already there, return 303 with the URL for the resource
+#     {enumerate the picture in data list}:
+#         if picture_in["id"] == picture["id"]:
+#             return {
+#                 "Message": f"{insert message here}"
+#             }, {insert HTTP code here}
+
+#     data.append(picture_in)
+#     return picture_in, {insert HTTP content created code here}
+
+# @app.route("{insert URL here}", methods={insert List of HTTP method here})
+# def {insert method name here}(id):
+
+#     # get data from the json body
+#     picture_in = {insert code to get json from request here}
+
+#     {insert code to enumerate picture in data list with index}:
+#         if picture["id"] == id:
+#             data[index] = picture_in
+#             return picture, {insert HTTP code here}
+
+#     return {"message": "insert error message here"}, {insert HTTP NOT FOUND code here}
+
+# @app.route("{insert URL here}", methods={insert List of HTTP method here})
+# def {insert method name here}(id):
+
+#     {insert code to enumerate pictures in data}:
+#         if picture["id"] == id:
+#             {insert code to delete picture from data}
+#             return "", {insert code to return HTTP code}
+
+#     return {"message": "{insert error message here}"}, {insert code to return HTTP code}
+
+# @app.route("/picture", methods=["GET"])
+# def get_pictures():
+#     return jsonify(data)
+
+# @app.route("/picture/<int:id>", methods=["GET"])
+# def get_picture_by_id(id):
+#     for picture in data:
+#         if picture["id"] == id:
+#             return picture
+#     return {"message": "picture not found"}, 404
+
+# @app.route("/picture", methods=["POST"])
+# def create_picture():
+
+#     # get data from the json body
+#     picture_in = request.json
+#     print(picture_in)
+
+#     # if the id is already there, return 303 with the URL for the resource
+#     for picture in data:
+#         if picture_in["id"] == picture["id"]:
+#             return {
+#                 "Message": f"picture with id {picture_in['id']} already present"
+#             }, 302
+
+#     data.append(picture_in)
+#     return picture_in, 201
+
+# @app.route("/picture/<int:id>", methods=["PUT"])
+# def update_picture(id):
+
+#     # get data from the json body
+#     picture_in = request.json
+
+#     for index, picture in enumerate(data):
+#         if picture["id"] == id:
+#             data[index] = picture_in
+#             return picture, 201
+
+#     return {"message": "picture not found"}, 404
+
+# @app.route("/picture/<int:id>", methods=["DELETE"])
+# def delete_picture(id):
+
+#     for picture in data:
+#         if picture["id"] == id:
+#             data.remove(picture)
+#             return "", 204
+
+#     return {"message": "picture not found"}, 404
